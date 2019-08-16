ebca273445c92cab8fd379d7d6bf72a18dc990d9
bugudiramu <ramubugudi4@gmail.com>
2019-08-16 17:09:25 +0530

    uploading images to firebase_storage without compressing and cropping

--- image_editor/lib/homepage.dart
+++ image_editor/lib/homepage.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:image_editor/cloud_uplode.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,10 +9,67 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  File _image;
+
+  getImageFile(ImageSource source) async {
+    var image = await ImagePicker.pickImage(source: source);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello"),
+    final double height = MediaQuery.of(context).size.height;
+    print(_image?.lengthSync());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("StringsWay Image Editor"),
+        centerTitle: true,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        height: height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: _image == null
+                  ? Text(
+                      "Image will be shown here".toUpperCase(),
+                    )
+                  : Image.file(
+                      _image,
+                      height: 300,
+                      width: 300,
+                    ),
+            ),
+            SizedBox(height: 20.0),
+            CloudUploader(file: _image),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FloatingActionButton.extended(
+            label: Text("Camera"),
+            onPressed: () => getImageFile(ImageSource.camera),
+            heroTag: UniqueKey(),
+            icon: Icon(Icons.camera),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          FloatingActionButton.extended(
+            label: Text("Gallery"),
+            onPressed: () => getImageFile(ImageSource.gallery),
+            heroTag: UniqueKey(),
+            icon: Icon(Icons.photo_library),
+          ),
+        ],
+      ),
     );
   }
 }
