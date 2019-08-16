dced915565b5957e36aa0db6a4bce87db2af429c
anurag71 <awasankar@gmail.com>
2019-08-16 11:32:39 +0530

    Added image picker and cropper

--- image_editor/lib/homepage.dart
+++ image_editor/lib/homepage.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,10 +10,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  File _image;
+
+  getImageFile(ImageSource source) async {
+
+    //Clicking or Picking from Gallery
+
+    var image = await ImagePicker.pickImage(source: source);
+
+    //Cropping the image
+
+    File croppedFile = await ImageCropper.cropImage(
+      sourcePath: image.path
+    );
+
+    //Compress the image
+
+    var result = await FlutterImageCompress.compressAndGetFile(
+      croppedFile.path,
+      croppedFile.path,
+      quality: 60,
+    );
+
+    setState(() {
+      _image = result;
+      print(_image.lengthSync());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello"),
+    print(_image?.lengthSync());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("StringsWay Sample"),
+      ),
+      body: Center(
+        child: _image == null
+            ? Text("Image")
+            : Image.file(
+          _image,
+          height: 200,
+          width: 200,
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton.extended(
+            label: Text("Camera"),
+            onPressed: () => getImageFile(ImageSource.camera),
+            heroTag: UniqueKey(),
+            icon: Icon(Icons.camera),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          FloatingActionButton.extended(
+            label: Text("Gallery"),
+            onPressed: () => getImageFile(ImageSource.gallery),
+            heroTag: UniqueKey(),
+            icon: Icon(Icons.photo_library),
+          )
+        ],
+      ),
     );
   }
 }
