b73ef9ec4948bd411ef2a826a0d22e883ac4409c
Ramu <37015092+bugudiramu@users.noreply.github.com>
2019-08-16 17:19:48 +0530

    Update main.dart

--- image_editor/lib/main.dart
+++ image_editor/lib/main.dart
@@ -7,7 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
+      theme: ThemeData.dark(),
     );
   }
 }
